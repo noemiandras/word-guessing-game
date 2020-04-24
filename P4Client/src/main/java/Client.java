4cb3a8a0c5089640d0f6a54f96803529a222906c
Rafiya Awan <rawan2901@gmail.com>
2020-04-24 16:56:05 -0500

    added Client class with basics

---
+++ P4Client/src/main/java/Client.java
@@ -0,0 +1,64 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.net.Socket;
+import java.util.function.Consumer;
+
+
+
+public class Client extends Thread{
+
+	
+	Socket socketClient;
+	
+	ObjectOutputStream out;
+	ObjectInputStream in;
+	
+	private Consumer<Serializable> callback;
+	
+	int portNum;
+	String ipAddr;
+	
+	Client(Consumer<Serializable> call, int portNum, String ipAddr){
+	
+		callback = call;
+		this.portNum = portNum;
+		this.ipAddr = ipAddr;
+	}
+	
+	public void run() {
+		
+		try {
+		socketClient= new Socket(ipAddr,portNum);
+	    out = new ObjectOutputStream(socketClient.getOutputStream());
+	    in = new ObjectInputStream(socketClient.getInputStream());
+	    socketClient.setTcpNoDelay(true);
+		}
+		catch(Exception e) {}
+		
+		while(true) {
+			 
+			try {
+			String message = in.readObject().toString();
+			callback.accept(message);
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	
+    }
+	
+	public void send(String data) {
+		
+		try {
+			out.writeObject(data);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+
+}
