86731b65a2446ee184c5f14f6c8321bb8e199b28
Rafiya Awan <rawan2901@gmail.com>
2020-04-27 17:32:57 -0500

    Added fields to WordInfo Serializable object

--- P4Client/src/main/java/WordInfo.java
+++ P4Client/src/main/java/WordInfo.java
@@ -4,5 +4,14 @@ public class WordInfo implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	int clientNum;
+	
+	String category;
+	char letterGuess;
+	int lengthOfWord;
+	boolean charInWord;
+	int letterPos;
+    int numGuesses;
+
 	
 }
